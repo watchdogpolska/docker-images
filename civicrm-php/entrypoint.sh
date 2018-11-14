ed9a1766ac8a36d93358f1d651fae252380b1919
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-11-14 03:19:39 +0100

    Add mod_auth_openidc

---
+++ civicrm-php/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z "$OIDCClientID" -o -z "$OIDCClientSecret" -o -z "$OIDCCryptoPassphrase" -o -z "$OIDCClaimHD" ]; then
+    echo "Missing environment variables. Required OIDCClientID, OIDCClientSecret, OIDCCryptoPassphrase and OIDCClaimHD.";
+    exit;
+fi;
+
+sed -e "s/{{OIDCClientID}}/$OIDCClientID/g" \
+    -e "s/{{OIDCClientSecret}}/$OIDCClientSecret/g" \
+    -e "s/{{OIDCCryptoPassphrase}}/$OIDCCryptoPassphrase/g" \
+    -e "s/{{OIDCClaimHD}}/$OIDCClaimHD/g" \
+    -i /etc/apache2/conf-enabled/mod_auth_openidc.conf;
+exec docker-php-entrypoint $@
