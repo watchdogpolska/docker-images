c495c3a5ec00801fbea1a4edc1d865562edae31b
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-11-27 10:48:05 +0100

    Disable mod_auth_openidc in civicrm

--- civicrm-php/entrypoint.sh
+++ civicrm-php/entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-set -e
+set -euxo pipefail;
 
-required_env="Required OIDCClientID, OIDCClientSecret, OIDCCryptoPassphrase and OIDCClaimHD.";
+# required_env="Required OIDCClientID, OIDCClientSecret, OIDCCryptoPassphrase and OIDCClaimHD.";
 
-[ -z "$OIDCClientID" ] && echo "Missing environment variable $OIDCClientID. $required_env" &&  exit 1 ;
-[ -z "$OIDCClientSecret" ] && echo "Missing environment variable $OIDCClientSecret. $required_env" &&  exit 1 ;
-[ -z "$OIDCCryptoPassphrase" ] && echo "Missing environment variable $OIDCCryptoPassphrase. $required_env" &&  exit 1 ;
-[ -z "$OIDCClaimHD" ] && echo "Missing environment variable $OIDCClaimHD. $required_env" &&  exit 1 ;
+# [ -z "$OIDCClientID" ] && echo "Missing environment variable $OIDCClientID. $required_env" &&  exit 1 ;
+# [ -z "$OIDCClientSecret" ] && echo "Missing environment variable $OIDCClientSecret. $required_env" &&  exit 1 ;
+# [ -z "$OIDCCryptoPassphrase" ] && echo "Missing environment variable $OIDCCryptoPassphrase. $required_env" &&  exit 1 ;
+# [ -z "$OIDCClaimHD" ] && echo "Missing environment variable $OIDCClaimHD. $required_env" &&  exit 1 ;
 
-sed -e "s/{{OIDCClientID}}/$OIDCClientID/g" \
-    -e "s/{{OIDCClientSecret}}/$OIDCClientSecret/g" \
-    -e "s/{{OIDCCryptoPassphrase}}/$OIDCCryptoPassphrase/g" \
-    -e "s/{{OIDCClaimHD}}/$OIDCClaimHD/g" \
-    -i /etc/apache2/conf-enabled/mod_auth_openidc.conf && \
-cat /etc/apache2/conf-enabled/mod_auth_openidc.conf && \
-apachectl configtest && \
+# sed -e "s/{{OIDCClientID}}/$OIDCClientID/g" \
+#     -e "s/{{OIDCClientSecret}}/$OIDCClientSecret/g" \
+#     -e "s/{{OIDCCryptoPassphrase}}/$OIDCCryptoPassphrase/g" \
+#     -e "s/{{OIDCClaimHD}}/$OIDCClaimHD/g" \
+#     -i /etc/apache2/conf-enabled/mod_auth_openidc.conf;
+
+# cat /etc/apache2/conf-enabled/mod_auth_openidc.conf
+
+apachectl configtest;
+
 exec docker-php-entrypoint $@
