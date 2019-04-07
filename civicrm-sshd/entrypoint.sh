acbfcb4b6cb8454ba693bc4f8c440dbd21a0e198
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-04-07 22:02:43 +0200

    Update civicrm-sshd for multi-user

--- civicrm-sshd/entrypoint.sh
+++ civicrm-sshd/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 set -e
 
+[ ! -f "$SSH_KEYS_DIR" ] && ssh-keygen -f "$SSH_KEYS_DIR/ssh_host_rsa_key" -N '' -t rsa
+[ ! -f "$SSH_KEYS_DIR" ] && ssh-keygen -f "$SSH_KEYS_DIR/ssh_host_ecdsa_key" -N '' -t ecdsa
+[ ! -f "$SSH_KEYS_DIR" ] && ssh-keygen -f "$SSH_KEYS_DIR/ssh_host_ed25519_key" -N '' -t ed25519
+
 [ -z "$GITHUB_USERS" ] && echo "Missing environment variable GITHUB_USERS." &&  exit 1 ;
-echo "$GITHUB_USERS" | sed "s/,/\n/g" | while read username; do
-    curl "https://github.com/$username.keys" -s >> ~/.ssh/authorized_keys;
+echo "$GITHUB_USERS" | sed "s/,/\n/g" | while read github_user; do
+	echo "$ACCESS_USER" | sed "s/,/\n/g" | while read access_user; do
+		homedir=$(getent passwd "$access_user" | cut -d':' -f 6)
+    curl "https://github.com/$github_user.keys" -s >> "$homedir/.ssh/authorized_keys";
+  done
 done;
 
-exec $@
+exec $@;
