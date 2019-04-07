6dabcf748b55c705ccd6bc2d2ebdbcf72c2a5d98
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-04-07 18:59:03 +0200

    Update civicrm-sshd

--- civicrm-sshd/entrypoint.sh
+++ civicrm-sshd/entrypoint.sh
@@ -2,12 +2,8 @@
 set -e
 
 [ -z "$GITHUB_USERS" ] && echo "Missing environment variable GITHUB_USERS." &&  exit 1 ;
-
-ls -lah /root/;
-
-echo "" > /root/.ssh/authorized_keys;
 echo "$GITHUB_USERS" | sed "s/,/\n/g" | while read username; do
-    curl "https://github.com/$username.keys" -s >> /root/.ssh/authorized_keys;
+    curl "https://github.com/$username.keys" -s >> ~/.ssh/authorized_keys;
 done;
 
 exec $@
