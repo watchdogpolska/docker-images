2acd3601ad9c6fcc452f2a1fe495b8d2a63dfd21
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-08-11 18:49:36 +0200

    Rewirte mixpanel proxy to Koa & add retry

--- mixpanel-proxy/index.js
+++ mixpanel-proxy/index.js
@@ -1,72 +1,57 @@
 'use strict';
-const http = require('http');
-const httpProxy = require('http-proxy');
 const url = require('url');
+const Koa = require('koa');
+const superagent = require('superagent');
+const Sentry = require('@sentry/node');
 
 const API_URL = process.env.MIXPANEL_API_URL || 'https://api.mixpanel.com';
 const PORT = process.env.PORT || 3000;
 const TRUNCATE_URL = process.env.TRUNCATE_URL || '^/mpanel';
 const parsed_url = url.parse(API_URL);
 
-if (process.env.SENTRY_DSN) {
-    require('@sentry/node').init({ dsn: process.env.SENTRY_DSN });
-}
+const agent = superagent.agent()
+    .use(require('superagent-logger')({ outgoing: true }))
+    .retry(3);
 
-const enrichOriginIp = (req) => {
-    const parsed_incoming_url = url.parse(req.url);
-    const searchParams = new URLSearchParams(parsed_incoming_url.query);
-    const data_b64 = searchParams.get('data');
-    const client_ip = req.headers['x-forwarded-for'];
+const app = new Koa();
+
+app.use(require('koa-logger')());
+
+app.use(async ctx => {
+    const client_ip = ctx.request.headers['x-forwarded-for'];
+    const data_b64 = ctx.query.data;
     if (data_b64 && client_ip) {
         const data = JSON.parse(Buffer.from(data_b64, 'base64').toString('utf-8'));
         data.$ip = client_ip;
         if (data.properties) {
             data.properties.ip = client_ip;
         }
-        const new_data_b64 = Buffer.from(JSON.stringify(data)).toString('base64');
-        searchParams.set('data', new_data_b64);
-        parsed_incoming_url.query = searchParams.toString();
-        req.url = parsed_incoming_url.format();
+        ctx.query.data = Buffer.from(JSON.stringify(data)).toString('base64');
     }
-};
+    const path = ctx.path.replace(new RegExp(TRUNCATE_URL, 'gm'), '');
+    const res = await agent[ctx.method.toLowerCase()](`${API_URL}/${path}`)
+        .query(ctx.query)
+        .set(ctx.request.headers)
+        .set({Host: parsed_url.hostname});
+    ctx.set(res.headers);
+    ctx.body = res.text;
+});
 
-const truncateOriginIp = (req) => {
-    if (TRUNCATE_URL) {
-        req.url = req.url.replace(new RegExp(TRUNCATE_URL, 'gm'), '');
+app.use(async (ctx, next) => {
+    try {
+        return await next();
+    } catch (err) {
+        ctx.status = err.status || 500;
+        ctx.body = err.message;
+        ctx.app.emit('error', err, ctx);
     }
-};
-
-const disableKeepAlive = (req) => {
-    req.headers['connection'] = 'close'
-};
-
-const proxy = httpProxy.createProxyServer({
-    changeOrigin: true
 });
 
-const server = http.createServer(function(req, res) {
-    req.headers.host = parsed_url.hostname;
-    enrichOriginIp(req);
-    truncateOriginIp(req);
-    disableKeepAlive(req);
-
-    proxy.web(req, res, {
-        target: API_URL,
-    });
-
-    proxy.on('error', (e) => {
-        const err = new Error('An exception coming from a proxy');
-        err.e = e;
-        throw err;
-    });
+app.on('error', (err) => {
+    console.log(err);
+    Sentry.captureException(err);
 });
 
-server.on('error', (e) => {
-    const err = new Error('An exception coming from a HTTP server');
-    err.e = e;
-    throw err;
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
