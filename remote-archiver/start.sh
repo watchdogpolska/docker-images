c4811ffee270409308890cfc03d2e577595e04bd
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-10-20 23:36:32 +0200

    remote-archiver: Logs current datasets

--- remote-archiver/start.sh
+++ remote-archiver/start.sh
@@ -4,4 +4,4 @@ echo "$(date -R): Started backup";
 ./sources/${BACKUP_SRC}.sh;
 echo "$(date -R): Finished backup";
 find /output -mtime +3 -mindepth 1 -print -delete;
-find /output -print
+find /output -type f -exec ls -lah {} \;
