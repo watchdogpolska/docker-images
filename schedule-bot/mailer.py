c378954152c34f6686ec53a9516b1cc433106ec4
kkm <kubasurdej@gmail.com>
2020-03-20 21:52:28 +0100

    Extract mailer functionality to another file

--- schedule-bot/mailer.py
+++ schedule-bot/mailer.py
@@ -1,9 +1,31 @@
 import os
 import sys
+import random
 
 from urllib.parse import urlparse
 from email.message import EmailMessage
 from smtplib import SMTP
+from jinja2 import Template, Environment, FileSystemLoader
+from html5print import HTMLBeautifier
+
+from dateutils import datetimeformat, structformat, struct_to_datetime
+
+
+def render_html(events, etr_events, feed_events, it_days):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    env = Environment(loader=FileSystemLoader(dir_path))
+    env.filters["datetimeformat"] = datetimeformat
+    env.filters["structformat"] = structformat
+
+    template = env.get_template("template.htm")
+    html = template.render(
+        events=events, etr_events=etr_events, feed_events=feed_events,
+        it_days=str(it_days)
+    )
+
+    pretty_html = HTMLBeautifier.beautify(html, 4)
+
+    return pretty_html
 
 
 def send_mail(subject, html):
@@ -21,3 +43,17 @@ def send_mail(subject, html):
     with SMTP(config.hostname, config.port or 587) as s:
         s.login(config.username, config.password)
         s.send_message(email)
+
+
+def random_email_subject():
+    subject_list = [
+        "Plany, plany, plany (na przyszły tydzień)",
+        "Ciekawy tydzień Stowarzyszenia",
+        "Tygodniowy plan pracy Stowarzyszenia",
+        "Tydzień Stowarzyszenia",
+        "Okazje od Stowarzyszenia w tym tygodniu",
+        "Okazje dla członków w tym tygodniu",
+        "Okazje dla członkiń w tym tygodniu",
+    ]
+
+    return random.choice(subject_list)
--- schedule-bot/main.py
+++ schedule-bot/main.py
@@ -1,34 +1,14 @@
 import os
 import sys
-import random
 import json
 import datetime
 from time import mktime
 
-from jinja2 import Template, Environment, FileSystemLoader
-from html5print import HTMLBeautifier
-
-from dateutils import datetimeformat, structformat, struct_to_datetime
 from ical import fetch_filtered_events
-from mailer import send_mail
+from mailer import send_mail, render_html, random_email_subject
 from feed import parse_feed
 
 ETR_WARSZAWA_URL = "https://raw.githubusercontent.com/ad-m/etr-warszawa-ical/master/648.ics"
-
-SUBJECT_LIST = [
-    "Plany, plany, plany (na przyszły tydzień)",
-    "Ciekawy tydzień Stowarzyszenia",
-    "Tygodniowy plan pracy Stowarzyszenia",
-    "Tydzień Stowarzyszenia",
-    "Okazje od Stowarzyszenia w tym tygodniu",
-    "Okazje dla członków w tym tygodniu",
-    "Okazje dla członkiń w tym tygodniu",
-]
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-env = Environment(loader=FileSystemLoader(dir_path))
-env.filters["datetimeformat"] = datetimeformat
-env.filters["structformat"] = structformat
 
 
 def main():
@@ -47,17 +27,13 @@ def main():
     feed_events = parse_feed("https://siecobywatelska.pl/feed/") + parse_feed(
         "https://informacjapubliczna.org/feed/"
     )
-    template = env.get_template("template.htm")
 
     it_days = (datetime.date(2020, 7, 1) - datetime.datetime.now().date()).days
 
-    html = template.render(
-        events=wd_events, etr_events=etr_events, feed_events=feed_events,
-        it_days=str(it_days)
-    )
-    pretty_html = HTMLBeautifier.beautify(html, 4)
-    subject = random.choice(SUBJECT_LIST)
-    return send_mail(subject, pretty_html)
+    html = render_html(wd_events, etr_events, feed_events, it_days)
+    subject = random_email_subject()
+
+    return send_mail(subject, html)
 
 
 if __name__ == "__main__":
