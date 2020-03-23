aec88b866d1c44ff35ef82365a46edf69468cdea
kkm <kubasurdej@gmail.com>
2020-03-23 19:22:32 +0100

    Refactor fb.py, format facebook posts

--- schedule-bot/lib/fb.py
+++ schedule-bot/lib/fb.py
@@ -1,24 +1,30 @@
 import os
 import requests
 from datetime import datetime, timedelta
+from textutils import put_links_in_anchors
 
 
 def get_facebook_posts(resource_id):
     FACEBOOK_ACCESS_TOKEN = os.environ['FACEBOOK_ACCESS_TOKEN']
 
     base_url = 'https://graph.facebook.com/v6.0/{}?fields={}&access_token={}'
-    fields = "posts"
-    url = base_url.format(page_id, fields, FACEBOOK_ACCESS_TOKEN)
+    fields = "posts{permalink_url,created_time,message}"
+    url = base_url.format(resource_id, fields, FACEBOOK_ACCESS_TOKEN)
+
     result = requests.get(url)
-    return result.json()['posts']['data']
+    posts = []
+    json = result.json()['posts']['data']
+    for i in range(len(json)):
+        post = json[i]
+        posts.append({
+            'id': post['id'],
+            'message': put_links_in_anchors(post['message']),
+            'created_time':  datetime.strptime(post['created_time'], "%Y-%m-%dT%H:%M:%S%z").date(),
+            'permalink_url': post['permalink_url']
+        })
+    return posts
 
 
 def filter_facebook_posts(posts, max_days=7):
     min_time = datetime.today() - timedelta(days=max_days)
-    return [p for p in posts if datetime.strptime(p['created_time'], "%Y-%m-%dT%H:%M:%S%z").date() >= min_time.date()]
-
-
-page_id = "SiecObywatelskaWatchdogPolska"
-posts = get_facebook_posts(page_id)
-filtered_posts = filter_facebook_posts(posts)
-print(filtered_posts)
+    return [p for p in posts if p['created_time'] >= min_time.date()]
