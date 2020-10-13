571da364696069e10c204a614418bfb3811e9eca
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-10-13 03:03:53 +0200

    Add GitHub issues list to email

--- schedule-bot/lib/main.py
+++ schedule-bot/lib/main.py
@@ -5,42 +5,42 @@ from ical import fetch_filtered_events
 from mailer import send_mail, render_html, random_email_subject
 from feed import parse_feed
 from twitter import authorize_twitter_api, get_user_tweets
+from github import fetch_latest_issues
 
-ETR_WARSZAWA_URL = "https://raw.githubusercontent.com/ad-m/etr-warszawa-ical/master/648.ics"
+ETR_WARSZAWA_URL = (
+    "https://raw.githubusercontent.com/ad-m/etr-warszawa-ical/master/648.ics"
+)
 
 
 def main():
     etr_events = fetch_filtered_events(ETR_WARSZAWA_URL)
-    etr_events = [
-        events for events in etr_events if "[J]" in events["summary"]]
+    etr_events = [events for events in etr_events if "[J]" in events["summary"]]
 
     wd_events = (
         fetch_filtered_events(os.environ["CALENDAR_URL"])
         if "CALENDAR_URL" in os.environ
         else []
     )
-    wd_events = [
-        x for x in wd_events if "Nieobecności/urlopy" not in x["categories"]]
+
+    wd_events = [x for x in wd_events if "Nieobecności/urlopy" not in x["categories"]]
 
     feed_events = parse_feed("https://siecobywatelska.pl/feed/") + parse_feed(
         "https://informacjapubliczna.org/feed/"
     )
 
-    twitter_api = (
-        authorize_twitter_api()
-        if "TWITTER_API_KEY" in os.environ
-        else False
+    twitter_api = authorize_twitter_api() if "TWITTER_API_KEY" in os.environ else False
+
+    tweets = get_user_tweets(twitter_api) if twitter_api else []
+
+    issues = fetch_latest_issues()
+
+    html = render_html(
+        wd_events=wd_events,
+        etr_events=etr_events,
+        feed_events=feed_events,
+        tweets=tweets,
+        issues=issues,
     )
-
-    tweets = (
-        get_user_tweets(twitter_api)
-        if twitter_api
-        else []
-    )
-
-    it_days = (datetime.date(2020, 7, 30) - datetime.datetime.now().date()).days
-
-    html = render_html(wd_events, etr_events, feed_events, it_days, tweets)
     subject = random_email_subject()
 
     return send_mail(subject, html)
